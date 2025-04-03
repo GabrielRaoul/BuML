617c0dbe75127cf5edfeb4bfc303b0fb7b3d56a7
Raoul Gabriel <r.gabriel@ci-tec.de>
2025-04-03 18:13:12 +0200

    update

--- plots.py
+++ plots.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import rasterio as rio
+import os
 
 def linear_dataset_plot(X,y):
     plt.figure(figsize=(15, 5))
